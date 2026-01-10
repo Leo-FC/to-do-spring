10269c85974111745db5b3cd7c85f527e4d71b6f
Leo-FC <leonardo.ferreiracontato1@gmail.com>
2026-01-10 15:49:15 -0300

    feat: UserDetailsServiceImpl and JWTUtil

---
+++ src/main/java/com/lfc/todosimple/service/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.lfc.todosimple.service;
+
+import com.lfc.todosimple.model.User;
+import com.lfc.todosimple.repository.UserRepository;
+import com.lfc.todosimple.security.UserSpringSecurity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByUsername(username);
+
+        if (Objects.isNull(user)) {
+            throw new UsernameNotFoundException("Usuario nao encontrado: " + username);
+        }
+
+        return new UserSpringSecurity(user.getId(), user.getUsername(), user.getPassword(), user.getProfiles());
+    }
+}
